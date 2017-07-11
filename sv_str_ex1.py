0b4449b1279fb6e1152d3b211169e3f90011d97b
s v <siva.vadakandara@gmail.com>
2017-07-11 14:19:22 -0700

    py2 or py3

--- sv_str_ex1.py
+++ sv_str_ex1.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 a = ("siva")
 b = ("network")
 c = ("security")
-d = raw_input("enter a name:")
+#d = raw_input("enter a name:")
 
+
+try:
+    # PY2
+    d = raw_input("Enter fourth name: ")
+except NameError:
+    # PY3
+    d= input("Enter fourth name: ")
 
 print()
-print("{:<30}".format(a))
-print("{:<30}".format(b))
-print("{:<30}".format(c))
-print("{:<30}".format(d))
+print("{:>30}".format(a))
+print("{:>30}".format(b))
+print("{:>30}".format(c))
+print("{:>30}".format(d))
 print()
